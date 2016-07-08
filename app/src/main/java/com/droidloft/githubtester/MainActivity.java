bd5c1257200b7903ee178e12eb6394be0d4cf079
Michael May <maydev99@gmail.com>
2016-07-08 18:50:32 -0400

    Added image and Animation

--- app/src/main/java/com/droidloft/githubtester/MainActivity.java
+++ app/src/main/java/com/droidloft/githubtester/MainActivity.java
@@ -2,12 +2,29 @@ package com.droidloft.githubtester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView)findViewById(R.id.imageView);
+
+        final Animation anim  = AnimationUtils.loadAnimation(this, R.anim.anim);
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv.startAnimation(anim);
+            }
+        });
     }
 }
